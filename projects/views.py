4ecf8f961029751ed55adab2da6002a9aea8fc8b
your name <aatran2021@gmail.com>
2023-03-06 17:25:15 -0800

    Feature 13 complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from projects.models import Project
 
@@ -6,5 +6,16 @@ from projects.models import Project
 @login_required
 def list_projects(request):
     projects = Project.objects.filter(owner=request.user)
-    context = {"projects": projects}
+    context = {
+        "projects": projects,
+    }
     return render(request, "projects/projects.html", context)
+
+
+@login_required
+def show_project(request, id):
+    project = get_object_or_404(Project, id=id)
+    context = {
+        "project_object": project,
+    }
+    return render(request, "projects/project_detail.html", context)
